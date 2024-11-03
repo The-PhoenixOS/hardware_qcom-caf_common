75ea27dc1962bfaa41f5ef827e25ee50b69e6403
Nikita Savchenko <NikitaSavc@yandex.ru>
2024-11-03 17:44:07 +0300

    qcom: Check TARGET_KERNEL_VERSION and add to the corresponding kernel family
    * Qualcomm updated kona and bengal platforms to 5.15 kernel version and Android T.
    * They requires new HALs, but hardware/qcom-caf/common doesn't provide for such platforms.
    * To avoid adding each platform separately, as is done in Ib3cf80c348face6eac8552c15859d1700dd60659, let's check TARGET_KERNEL_VERSION and add to the corresponding kernel family
    * Also, the kona platform does not have TARGET_BOARD_SUFFIX, like bengal and we can't distinguish kona with 4.19 kernel version and 5.15 kernel version except TARGET_KERNEL_VERSION (iirc)
    
    Change-Id: I6791504f3289e876f4611108783562b9d83682e0
    Signed-off-by: Nikita Savchenko <NikitaSavc@yandex.ru>

--- qcom_defs.mk
+++ qcom_defs.mk
@@ -3,11 +3,20 @@ MSMSTEPPE := sm6150
 TRINKET := trinket #SM6125
 
 # UM families
-UM_3_18_FAMILY := msm8996 msm8937 msm8953
-UM_4_4_FAMILY := msm8998 sdm660
-UM_4_9_FAMILY := sdm845 sdm710
-UM_4_14_FAMILY := msmnile $(MSMSTEPPE) $(TRINKET) atoll
-UM_4_19_FAMILY := kona lito bengal
-UM_5_4_FAMILY := lahaina holi
-UM_5_10_FAMILY := taro parrot
-UM_5_15_FAMILY := kalama crow
+ifneq (,$(filter 3.18, $(TARGET_KERNEL_VERSION)))
+  UM_3_18_FAMILY := $(TARGET_BOARD_PLATFORM)
+else ifneq (,$(filter 4.4, $(TARGET_KERNEL_VERSION)))
+  UM_4_4_FAMILY := $(TARGET_BOARD_PLATFORM)
+else ifneq (,$(filter 4.9, $(TARGET_KERNEL_VERSION)))
+  UM_4_9_FAMILY := $(TARGET_BOARD_PLATFORM)
+else ifneq (,$(filter 4.14, $(TARGET_KERNEL_VERSION)))
+  UM_4_14_FAMILY := $(TARGET_BOARD_PLATFORM)
+else ifneq (,$(filter 4.19, $(TARGET_KERNEL_VERSION)))
+  UM_4_19_FAMILY := $(TARGET_BOARD_PLATFORM)
+else ifneq (,$(filter 5.4, $(TARGET_KERNEL_VERSION)))
+  UM_5_4_FAMILY := $(TARGET_BOARD_PLATFORM)
+else ifneq (,$(filter 5.10, $(TARGET_KERNEL_VERSION)))
+  UM_5_10_FAMILY := $(TARGET_BOARD_PLATFORM)
+else ifneq (,$(filter 5.15, $(TARGET_KERNEL_VERSION)))
+  UM_5_15_FAMILY := $(TARGET_BOARD_PLATFORM)
+endif
